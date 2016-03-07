2f87e028dd6ba37c54bdc3a423cdbce0e731b7ae
NeeKin <njgzs360@gmail.com>
2016-03-07 00:20:18 +0000

    first commit

--- db/migrate/20160306231337_create_consignees.rb
+++ db/migrate/20160306231337_create_consignees.rb
@@ -2,7 +2,7 @@ class CreateConsignees < ActiveRecord::Migration
   def change
     create_table :consignees do |t|
       t.string :name  #收货人名字
-      t.sting :phone  #联系方式
+      t.string :phone  #联系方式
       t.integer :company_id  #公司信息
 
       t.timestamps null: false
